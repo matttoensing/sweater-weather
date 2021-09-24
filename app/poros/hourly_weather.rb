88bc453b5f71d2f0892b4b08bec8e81ecf25e9ef
Matt Toensing <matthew.toensing@gmail.com>
2021-09-24 07:45:32 -0700

    Reformat hourly service for new weather service json structure

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -2,9 +2,13 @@ class HourlyWeather
   attr_reader :time, :temperature, :conditions, :icon
 
   def initialize(info)
-    @time = info[:time]
-    @temperature = info[:temp_f]
-    @conditions = info[:condition][:text]
-    @icon = info[:condition][:icon]
+    @time = info[:dt]
+    @temperature = info[:temp]
+    @conditions = info[:weather][0][:description]
+    @icon = format_icon(info[:weather][0][:icon])
+  end
+
+  def format_icon(icon)
+    "http://openweathermap.org/img/w/#{icon}.png"
   end
 end
--- spec/poros/hourly_weather_spec.rb
+++ spec/poros/hourly_weather_spec.rb
@@ -3,20 +3,35 @@ require 'rails_helper'
  RSpec.describe HourlyWeather do
    it 'exists and has attributes' do
     attributes =  {
-       time: "2020-10-01",
-       temp_f: 64.0,
-       condition: {
-         text: 'Sunny',
-         icon: "//cdn.weatherapi.com/weather/64x64/night/113.png"
-       }
-     }
+            dt: 1632492000,
+            temp: 299.01,
+            feels_like: 298.88,
+            pressure: 1012,
+            humidity: 47,
+            dew_point: 286.85,
+            uvi: 0,
+            clouds: 42,
+            visibility: 10000,
+            wind_speed: 1.72,
+            wind_deg: 47,
+            wind_gust: 1.91,
+            weather: [
+                {
+                    id: 802,
+                    main: "Clouds",
+                    description: "scattered clouds",
+                    icon: "03d"
+                }
+            ],
+            pop: 0.19
+        }
 
      hourly_weather = HourlyWeather.new(attributes)
 
      expect(hourly_weather).to be_an_instance_of(HourlyWeather)
-     expect(hourly_weather.time).to eq(attributes[:time])
-     expect(hourly_weather.temperature).to eq(attributes[:temp_f])
-     expect(hourly_weather.conditions).to eq(attributes[:condition][:text])
-     expect(hourly_weather.icon).to eq(attributes[:condition][:icon])
+     expect(hourly_weather.time).to eq(attributes[:dt])
+     expect(hourly_weather.temperature).to eq(attributes[:temp])
+     expect(hourly_weather.conditions).to eq(attributes[:weather][0][:description])
+     expect(hourly_weather.icon).to eq("http://openweathermap.org/img/w/03d.png")
    end
  end
