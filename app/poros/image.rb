7fc68f7d5afc16c0e77a5eaa9c7855e6f7d37859
Matt Toensing <matthew.toensing@gmail.com>
2021-09-24 16:16:22 -0700

    Create image poro with attributes and passing test

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -1,7 +1,11 @@
 class Image
-  attr_reader :id
+  attr_reader :id, :image_url, :author, :author_url, :source
 
   def initialize(data)
     @id = nil
+    @image_url = data[:results][0][:urls][:full]
+    @author = data[:results][0][:user][:name]
+    @author_url = data[:results][0][:user][:portfolio_url]
+    @source = 'unsplash.com'
   end
 end
