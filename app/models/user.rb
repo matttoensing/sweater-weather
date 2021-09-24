1b603fdc44a22b677bfd65b970d5ba2baf9fd584
Matt Toensing <matthew.toensing@gmail.com>
2021-09-23 17:30:17 -0700

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :password, presence: true
   validates :password, confirmation: { case_sensitive: true }
+  validates :api_key, confirmation: { case_sensitive: true }
 
   has_secure_password
-  has_secure_token :api_key, confirmation: { case_sensitive: true }
+  has_secure_token :api_key
 end
