b8b75508c771543f6e4096478ddca8f2295380e0
Matt Toensing <matthew.toensing@gmail.com>
2021-09-23 19:19:45 -0700

    Complete daily weather poro with test

---
+++ app/poros/daily_weather.rb
@@ -0,0 +1,19 @@
+class DailyWeather
+  attr_reader :date,
+              :sunrise,
+              :sunset,
+              :max_temp,
+              :min_temp,
+              :conditions,
+              :icon
+
+  def initialize(info)
+    @date = info[:date]
+    @sunrise = info[:day][:astro][:sunrise]
+    @sunset = info[:day][:astro][:sunset]
+    @max_temp = info[:day][:maxtemp_f]
+    @min_temp = info[:day][:mintemp_f]
+    @conditions = info[:day][:condition][:text]
+    @icon = info[:day][:condition][:icon]
+  end
+end
---
+++ spec/poros/daily_weather_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+ RSpec.describe DailyWeather do
+   it 'exists and has attributes' do
+     attributes = {
+       date: '2020-10-01',
+       day: {
+       maxtemp_f: 84.0,
+       mintemp_f: 54.3,
+       condition:{
+         text: 'Partly Cloudy',
+       icon: "//cdn.weatherapi.com/weather/64x64/day/116.png"
+     },
+     astro: {
+       sunrise: "06:50 AM",
+       sunset: "06:56 PM",
+     }
+   }
+   }
+
+     daily_weather = DailyWeather.new(attributes)
+
+     expect(daily_weather).to be_an_instance_of(DailyWeather)
+     expect(daily_weather.date).to eq(attributes[:date])
+     expect(daily_weather.sunrise).to eq(attributes[:day][:astro][:sunrise])
+     expect(daily_weather.sunset).to eq(attributes[:day][:astro][:sunset])
+     expect(daily_weather.max_temp).to eq(attributes[:day][:maxtemp_f])
+     expect(daily_weather.min_temp).to eq(attributes[:day][:mintemp_f])
+     expect(daily_weather.conditions).to eq(attributes[:day][:condition][:text])
+     expect(daily_weather.icon).to eq(attributes[:day][:condition][:icon])
+   end
+ end
