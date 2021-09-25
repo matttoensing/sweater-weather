ab4f85b655d991f9ad8b7a38ea4a95489819a3c8
Matt Toensing <matthew.toensing@gmail.com>
2021-09-24 20:18:56 -0700

    Add private method for converting user attributes from json to ruby

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    req = user_attributes
+    user = User.create(email: req[:email], password: req[:password], password_confirmation: req[:password_confirmation])
+    json_response(UserSerializer.new(user))
+  end
 
-    user = request.body.read
-    require "pry"; binding.pry
+  private
+
+  def user_attributes
+    json = request.body.read
+    req = json_request(json)
   end
 end
