1bf68157abc9dba5f0a5e1bb7a81a80c408d8e6d
Matt Toensing <matthew.toensing@gmail.com>
2021-09-24 19:54:41 -0700

    Successfully write test for passing body trhough params

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, :type => :controller do
+  it 'can register a new user' do
+    user_body = {
+      email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password"
+    }
+
+
+    headers = {"CONTENT_TYPE" => "application/json", "Accept": "application/json"}
+
+    post :create, params: {}, body: user_body.to_json, as: :json
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    user = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(user).to have_key(:type)
+    expect(user).to have_key(:id)
+    expect(user).to have_key(:attributes)
+    expect(user[:attributes]).to have_key(:email)
+    expect(user[:attributes][:email]).to eq("whatever@example.com")
+    expect(user[:attributes]).to have_key(:api_key)
+  end
+end
