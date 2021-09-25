cbe2171e510a92f5069781517966d6ad0546d9d9
Matt Toensing <matthew.toensing@gmail.com>
2021-09-25 09:29:31 -0700

    Remove private method and place in users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
-
-  after_action :set_code_on_create, only: [:create]
-
-  private
-
-  def set_code_on_create
-    response.status = 201 if response.status == 200
-  end
-
-  def req
-    json = request.body.read
-    return nil if json.empty?
-
-    json_request(json)
-  end
 end
