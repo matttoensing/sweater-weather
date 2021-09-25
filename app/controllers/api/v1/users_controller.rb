2a74d126f0a61a06bc1ae6ca250cbba458e257f2
Matt Toensing <matthew.toensing@gmail.com>
2021-09-25 09:30:17 -0700

    Add private method user params for creating a user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  after_action :set_code_on_create, only: [:create]
+
   def create
-    user = User.create(email: req[:email], password: req[:password], password_confirmation: req[:password_confirmation])
+    user = User.create(user_params)
 
     if user.save
       json_response(UserSerializer.new(user))
     else
       json_response(ErrorMessage.bad_credentials_for_user, :bad_request)
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+  def set_code_on_create
+    response.status = 201 if response.status == 200
   end
 end
