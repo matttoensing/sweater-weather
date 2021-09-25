41ae12bb3cc4e85627f2e6ab935117d82abb3315
Matt Toensing <matthew.toensing@gmail.com>
2021-09-25 14:24:25 -0700

    Add test for getting travel info between 2 locations

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -14,4 +14,21 @@ require 'rails_helper'
      expect(json[:results][0][:locations][0][:latLng]).to have_key(:lng)
      expect(json[:results][0][:locations][0][:latLng][:lng].class).to eq(Float)
    end
+
+   it 'can get information for directions between two locations', :vcr do
+     json = GeocodeService.get_directions_info('boulder,co', 'denver,co')
+
+     expect(json).to have_key(:route)
+     expect(json[:route].class).to eq(Hash)
+     expect(json[:route]).to have_key(:formattedTime)
+     expect(json[:route][:formattedTime].class).to eq(String)
+
+
+     expect(json[:route]).to have_key(:locations)
+     expect(json[:route][:locations].class).to eq(Array)
+     expect(json[:route][:locations][0]).to have_key(:adminArea5)
+     expect(json[:route][:locations][0][:adminArea5].class).to eq(String)
+     expect(json[:route][:locations][0]).to have_key(:adminArea3)
+     expect(json[:route][:locations][0][:adminArea3].class).to eq(String)
+   end
  end
