d8f8fe56fc4c43d8708d9f7b45043121c59df455
Matt Toensing <matthew.toensing@gmail.com>
2021-09-25 16:36:54 -0700

    Create road trip poro with tests and attributes

---
+++ spec/poros/roadtrip_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+ RSpec.describe Roadtrip do
+   it 'exists and has attributes' do
+     attributes = {
+      start_city: "Denver, CO",
+      end_city: "Estes Park, CO",
+      travel_time: "2 hours, 13 minutes",
+      weather_at_eta: {
+        temperature: 59.4,
+        conditions: "partly cloudy with a chance of meatballs"
+      }
+    }
+
+    roadtrip = Roadtrip.new(attributes)
+
+    expect(roadtrip).to be_an_instance_of(Roadtrip)
+    expect(roadtrip.start_city).to eq(attributes[:start_city])
+    expect(roadtrip.end_city).to eq(attributes[:end_city])
+    expect(roadtrip.travel_time).to eq(attributes[:travel_time])
+    expect(roadtrip.weather_at_eta).to eq(attributes[:weather_at_eta])
+   end
+ end
