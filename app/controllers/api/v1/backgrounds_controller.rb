cec18386f17d82bf2182b3c64c32ca7479f88d0e
Matt Toensing <matthew.toensing@gmail.com>
2021-09-24 17:02:55 -0700

    Add sad path testing and conditionals for background

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
+    if params[:location]
     image = ImageFacade.create_image(params[:location])
     json_response(ImageSerializer.new(image))
+  else
+    json_response(ErrorMessage.location_not_included_error, :bad_request)
+  end
   end
 end
--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
- RSpec.describe 'backgrounds API' do
-   it 'can send a background image for a specific location', :vcr do
-     get '/api/v1/backgrounds', params: { location: 'denver,co'}
+RSpec.describe 'backgrounds API' do
+  describe 'happy path' do
+    it 'can send a background image for a specific location', :vcr do
+      get '/api/v1/backgrounds', params: { location: 'denver,co'}
 
-     expect(response).to be_successful
+      expect(response).to be_successful
 
-     image = JSON.parse(response.body, symbolize_names: true)[:data]
+      image = JSON.parse(response.body, symbolize_names: true)[:data]
 
-     expect(image).to have_key(:id)
-     expect(image).to have_key(:type)
-     expect(image).to have_key(:attributes)
-     expect(image[:attributes]).to have_key(:image_url)
-     expect(image[:attributes]).to have_key(:location)
-     expect(image[:attributes]).to have_key(:author)
-     expect(image[:attributes]).to have_key(:author_url)
-     expect(image[:attributes]).to have_key(:source)
-   end
- end
+      expect(image).to have_key(:id)
+      expect(image).to have_key(:type)
+      expect(image).to have_key(:attributes)
+      expect(image[:attributes]).to have_key(:image_url)
+      expect(image[:attributes]).to have_key(:location)
+      expect(image[:attributes]).to have_key(:author)
+      expect(image[:attributes]).to have_key(:author_url)
+      expect(image[:attributes]).to have_key(:source)
+    end
+  end
+
+  describe 'sad path' do
+    it 'will throw an error if no location is present' do
+      get '/api/v1/backgrounds'
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to have_key(:error)
+      expect(error).to have_key(:message)
+    end
+  end
+end
