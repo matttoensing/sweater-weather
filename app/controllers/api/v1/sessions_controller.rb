0dce88f437c6ad381c119f49fba3eb4c30e549b3
Matt Toensing <matthew.toensing@gmail.com>
2021-09-25 09:30:38 -0700

    Change format and add user params private method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: req[:email])
-    if !user.nil? && user.authenticate(req[:password])
+    user = User.find_by(email: params[:email])
+    if !user.nil? && user.authenticate(params[:password])
       json_response(UserSerializer.new(user))
     else
      json_response(ErrorMessage.bad_credentials_for_user, :bad_request)
