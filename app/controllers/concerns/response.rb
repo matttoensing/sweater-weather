8b6bebdaac05e3fc4b1e09069aef7b1c1992336d
Matt Toensing <matthew.toensing@gmail.com>
2021-09-24 20:19:24 -0700

    Add json request method in module

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,8 @@ module Response
   def json_response(object, status = :ok)
     render(json: object, status: status)
   end
+
+  def json_request(request)
+    JSON.parse(request, symbolize_names: true)
+  end
 end
