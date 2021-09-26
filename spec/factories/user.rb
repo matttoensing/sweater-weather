82c3d50cd8628521f62a182a8a19195325e28e80
Matt Toensing <matthew.toensing@gmail.com>
2021-09-26 11:23:00 -0700

    Hardcode api key in factory to precent test failures

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
-    api_key { Faker::String.random(length: 12..18) }
+    api_key { 'alsjdf09sadfkjashdf0ysd09832jb' }
   end
 end
