ff07625a57d3549aa7c53f3378bda707e3af4382
Matt Toensing <matthew.toensing@gmail.com>
2021-09-27 09:41:37 -0700

    Add book search facade and render json response for book search

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
     location = GeocodeFacade.create_location(params[:location])
-    book_search = BookSearchFacade.create_book_search()
+    book_search = BookSearchFacade.create_book_search(params[:location], params[:quantity], location.latitude, location.longitude)
+    json_response(BooksSerializer.new(book_search))
   end
 end
