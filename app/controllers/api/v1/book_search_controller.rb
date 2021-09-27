3b8cdb8722e944df371fcd4067ef174977be36f3
Matt Toensing <matthew.toensing@gmail.com>
2021-09-27 09:30:19 -0700

    WIP

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-
+    location = GeocodeFacade.create_location(params[:location])
+    book_search = BookSearchFacade.create_book_search()
   end
-end 
+end
