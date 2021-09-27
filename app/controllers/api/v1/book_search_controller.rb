44165ca95ebcab677e5b300af0ecce7450fe965a
Matt Toensing <matthew.toensing@gmail.com>
2021-09-27 10:36:06 -0700

    Add conditional for edge cases

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    location = GeocodeFacade.create_location(params[:location])
-    book_search = BookSearchFacade.create_book_search(params[:location], params[:quantity], location.latitude, location.longitude)
-    json_response(BooksSerializer.new(book_search))
+    if location_and_quantity_present?
+      location = GeocodeFacade.create_location(params[:location])
+      book_search = BookSearchFacade.create_book_search(params[:location], params[:quantity], location.latitude, location.longitude)
+      json_response(BooksSerializer.new(book_search))
+    else
+      json_response(ErrorMessage.book_search_error, :bad_request)
+    end
+  end
+
+  private
+
+  def location_and_quantity_present?
+    params[:quantity].present? && params[:location].present?
   end
 end
