933d26849801c1d057237d7e41f1ec2880650c2a
Matt Toensing <matthew.toensing@gmail.com>
2021-09-27 10:05:02 -0700

    Correct test expectations after changes made in weather poro

--- spec/facades/book_search_facade_spec.rb
+++ spec/facades/book_search_facade_spec.rb
@@ -16,7 +16,7 @@ require 'rails_helper'
      expect(book_search.destination).to eq(query)
      expect(book_search.forecast).to be_an_instance_of(Weather)
      expect(book_search.forecast.summary).to eq("scattered clouds")
-     expect(book_search.forecast.temperature).to eq(79.8)
+     expect(book_search.forecast.temperature).to eq("80 F")
      expect(book_search.total_books_found).to eq(book_data[:numFound])
 
      book_search.books.each do |book|
