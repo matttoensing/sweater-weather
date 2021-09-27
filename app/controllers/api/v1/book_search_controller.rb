23b76faffcb22ada1797831ca6fc76e6b6199024
Matt Toensing <matthew.toensing@gmail.com>
2021-09-27 10:41:19 -0700

    Add line break for reading

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::BookSearchController < ApplicationController
     if location_and_quantity_present?
       location = GeocodeFacade.create_location(params[:location])
       book_search = BookSearchFacade.create_book_search(params[:location], params[:quantity], location.latitude, location.longitude)
+
       json_response(BooksSerializer.new(book_search))
     else
       json_response(ErrorMessage.book_search_error, :bad_request)
