e5491d1541c929e2dd42b049297da4bd48b3f26a
Matt Toensing <matthew.toensing@gmail.com>
2021-09-28 13:11:24 -0700

    Remove comments

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe Forecast do
     weather = Forecast.new(attributes)
 
     expect(weather).to be_an_instance_of(Forecast)
-    # expect(weather.id).to eq(nil)
-    #
-    # expect(weather.current_weather).to be_an_instance_of(CurrentWeather)
-    #
-    # expect(weather.daily_weather.count).to eq(5)
-    # weather.daily_weather.each do |daily|
-    #   expect(daily).to be_an_instance_of(DailyWeather)
-    # end
-    #
-    # expect(weather.hourly_weather.count).to eq(8)
-    # weather.hourly_weather.each do |hourly|
-    #   expect(hourly).to be_an_instance_of(HourlyWeather)
+    expect(weather.id).to eq(nil)
+
+    expect(weather.current_weather).to be_an_instance_of(CurrentWeather)
+
+    expect(weather.daily_weather.count).to eq(5)
+    weather.daily_weather.each do |daily|
+      expect(daily).to be_an_instance_of(DailyWeather)
+    end
+
+    expect(weather.hourly_weather.count).to eq(8)
+    weather.hourly_weather.each do |hourly|
+      expect(hourly).to be_an_instance_of(HourlyWeather)
   end
 end
