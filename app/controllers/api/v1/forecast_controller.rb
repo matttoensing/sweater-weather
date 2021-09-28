ddd09a5ec72cb2eb257b91ca01aa6963331aaf35
Matt Toensing <matthew.toensing@gmail.com>
2021-09-28 13:17:42 -0700

    Include tests for conditional for unit param values

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    if params[:location]
+    if params[:location] && !params[:units].present?
       location = GeocodeFacade.create_location(params[:location])
       forecast = ForecastFacade.create_weather(location.latitude, location.longitude)
+      json_response(ForecastSerializer.new(forecast))
+    elsif params[:location] && params[:units]
+      location = GeocodeFacade.create_location(params[:location])
+      forecast = ForecastFacade.create_weather(location.latitude, location.longitude, params[:units])
       json_response(ForecastSerializer.new(forecast))
     else
       json_response(ErrorMessage.location_not_included_error, :bad_request)
