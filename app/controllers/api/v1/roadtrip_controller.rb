ffe08c0c6d4234ce72a3e28c65188066fc141908
Matt Toensing <matthew.toensing@gmail.com>
2021-09-28 10:03:33 -0700

    Change conditional to include unauthorized request instead of bad request

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::RoadtripController < ApplicationController
 
   def create
     user = User.find_by(api_key: params[:api_key])
-    if user.nil?
-      json_response(ErrorMessage.api_key_invalid, :bad_request)
-    elsif api_key_present? && origin_and_destination_params_present? && !user.nil?
+    if user.nil? || api_key_not_present?
+      json_response(ErrorMessage.api_key_invalid, :unauthorized)
+    elsif origin_and_destination_params_present?
       roadtrip = RoadtripFacade.create_roadtrip(params[:origin], params[:destination])
       return json_response(ErrorMessage.roadtrip_route_not_available, :bad_request) if roadtrip.nil?
 
@@ -17,14 +17,8 @@ class Api::V1::RoadtripController < ApplicationController
 
   private
 
-  # def authenticate
-  #   authenticate_with_http_token do |token, _options|
-  #     @user = User.find_by!(api_key: token)
-  #   end
-  # end
-
-  def api_key_present?
-    params[:api_key].present?
+  def api_key_not_present?
+    !params[:api_key].present?
   end
 
   def origin_and_destination_params_present?
