e2b9e0056d2e373e2d5b0c2571c81999226763bb
Matt Toensing <matthew.toensing@gmail.com>
2021-09-28 13:12:14 -0700

    Add imperial as default unit argument and ability to pass metric into methods

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,16 +1,8 @@
 class Forecast
-  attr_reader :current_weather,
-  :daily_weather,
-  :hourly_weather,
-  :id
-
-  #   def initialize(data)
-  #     @id = nil
-  #     @current_weather = data[:current_weather]
-  #     @daily_weather = data[:daily_weather]
-  #     @hourly_weather = data[:hourly_weather]
-  #   end
-  # end
+  attr_reader :id,
+              :current_weather,
+              :daily_weather,
+              :hourly_weather
 
 
   def initialize(data)
@@ -25,7 +17,7 @@ class Forecast
       DailyWeather.new(details)
     end
   end
-  
+
   def create_hourly_weather(data)
     data[1..8].map do |details|
       HourlyWeather.new(details)
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,9 +1,9 @@
 class WeatherService < BaseService
-  def self.get_weather_info(lat, lon)
+  def self.get_weather_info(lat, lon, units = 'imperial')
     response = conn('http://api.openweathermap.org').get('/data/2.5/onecall') do |f|
       f.params['lat'] = lat
       f.params['lon'] = lon
-      f.params['units'] = 'imperial'
+      f.params['units'] = units
       f.params['appid'] = ENV['weather_key']
     end
     get_json(response)
